fe5ce3eb3f60de623c15baf7139e7ff53f434053
michaelr3207 <michael--robinson321@hotmail.com>
2024-03-25 18:52:46 +0000

    squares added to UI

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import './style.css';
 
 
+function createSquares(index){
+
+    let squares = 12;
+    const container = document.getElementById('grid' + index);
+    container.style.display = "block";
+
+    for(let i = 0; i < squares; i ++){
+        const newRow = document.createElement('div');
+        newRow.classList.add('row');
+        newRow.style.display = "flex";
+        for(let j = 0; j < squares; j ++){
+            const newSquare = document.createElement('div');
+            newSquare.classList.add('square');
+            newRow.appendChild(newSquare);
+        }
+        container.appendChild(newRow);
+    }
+}
+
+function populateBothGrids() {
+    const playerOneIndex = 1;
+    const playerTwoIndex = 2;
+    createSquares(playerOneIndex);
+    createSquares(playerTwoIndex);
+}
+
+populateBothGrids();
